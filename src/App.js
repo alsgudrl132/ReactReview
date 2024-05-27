9ae15727abc0d00e067f6f4cd6762ff6289acbba
alsgudrl132 <alsgudrl132@gmail.com>
2024-05-27 13:12:42 +0900

    2024-05-27 ReactReview

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import About from './pages/About';
 import Home from './pages/Home';
 import Counter from './pages/Counter';
 import Input from './pages/Input';
+import Input2 from './pages/Input2';
+import UserList from './pages/List';
 function App() {
     return (
         <div className="App">
@@ -13,12 +15,16 @@ function App() {
                 <Link to="/about">About</Link>
                 <Link to="/counter">Counter</Link>
                 <Link to="/input">Input</Link>
+                <Link to="/input2">Input2</Link>
+                <Link to="/list">List</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/counter" element={<Counter />} />
                 <Route path="/input" element={<Input />} />
+                <Route path="/input2" element={<Input2 />} />
+                <Route path="/list" element={<UserList />} />
             </Routes>
         </div>
     );
